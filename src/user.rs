b1e88731c935770ad0d4d9650597ef43dec0320f
yozhgoor <yozhgoor@outlook.com>
2022-03-07 18:42:32 +0100

    `cargo fmt`

--- src/user.rs
+++ src/user.rs
@@ -2,7 +2,7 @@ use crate::data;
 use cursive::{
     event::Key::Esc,
     view::{Nameable, Resizable},
-    views::{Button, Dialog, DummyView, LinearLayout, OnEventView, SelectView, TextView, EditView },
+    views::{Button, Dialog, DummyView, EditView, LinearLayout, OnEventView, SelectView, TextView},
     Cursive,
 };
 
@@ -67,35 +67,31 @@ pub fn user_welcome(siv: &mut Cursive) {
 
     let welcome = TextView::new(format!("Hello {}!", user.name));
 
-    siv.add_layer(
-        Dialog::around(welcome)
-            .button("Back", user_list)
-    );
+    siv.add_layer(Dialog::around(welcome).button("Back", user_list));
 }
 
 fn create_user(siv: &mut Cursive) {
-    siv.add_layer(
-        OnEventView::new(
-            Dialog::around(
-                LinearLayout::vertical()
+    siv.add_layer(OnEventView::new(
+        Dialog::around(
+            LinearLayout::vertical()
                 .child(TextView::new("Choose the name of the user"))
                 .child(DummyView)
                 .child(EditView::new().on_submit(|s, item| {
                     s.with_user_data(|data: &mut data::ProgramData| {
-                        data.user_list.insert(item.to_string(), User::new(item.to_string()))
+                        data.user_list
+                            .insert(item.to_string(), User::new(item.to_string()))
                     });
                     s.pop_layer();
                     s.pop_layer();
                     user_list(s);
                 })),
-            )
-            .title("Create a new user")
-            .button("Back", |s| {
-                s.pop_layer();
-                user_list(s)
-            }),
         )
-    )
+        .title("Create a new user")
+        .button("Back", |s| {
+            s.pop_layer();
+            user_list(s)
+        }),
+    ))
 }
 
 fn rename_user(siv: &mut Cursive) {
@@ -103,67 +99,76 @@ fn rename_user(siv: &mut Cursive) {
         OnEventView::new(
             Dialog::around(
                 LinearLayout::vertical()
-                .child(TextView::new("Choose a new name"))
-                .child(DummyView)
-                .child(EditView::new().on_submit(|s, item| {
-                    let selected = s.call_on_name("user_select", |view: &mut SelectView| {
-                        let (slug, _user) = view
-                            .get_item(
-                                view.selected_id().expect("cannot get the ID of the selected item"),
-                            )
-                            .expect("cannot get selected item");
+                    .child(TextView::new("Choose a new name"))
+                    .child(DummyView)
+                    .child(EditView::new().on_submit(|s, item| {
+                        let selected = s
+                            .call_on_name("user_select", |view: &mut SelectView| {
+                                let (slug, _user) = view
+                                    .get_item(
+                                        view.selected_id()
+                                            .expect("cannot get the ID of the selected item"),
+                                    )
+                                    .expect("cannot get selected item");
 
-                        slug.to_string()
-                    })
-                    .expect("cannot call user_select");
+                                slug.to_string()
+                            })
+                            .expect("cannot call user_select");
 
-                    s.with_user_data(|data: &mut data::ProgramData| {
-                        let mut user = data
-                            .user_list
-                            .remove(&selected)
-                            .expect("cannot remove user from user_list");
+                        s.with_user_data(|data: &mut data::ProgramData| {
+                            let mut user = data
+                                .user_list
+                                .remove(&selected)
+                                .expect("cannot remove user from user_list");
 
-                        user.name = item.to_string();
-                        data.user_list.insert(user.name.clone(), user)
-                    });
-                    s.pop_layer();
-                    s.pop_layer();
-                    user_list(s)
-                })),
+                            user.name = item.to_string();
+                            data.user_list.insert(user.name.clone(), user)
+                        });
+                        s.pop_layer();
+                        s.pop_layer();
+                        user_list(s)
+                    })),
+            )
+            .title("Rename a user")
+            .button("Back", |s| {
+                s.pop_layer();
+            }),
         )
-        .title("Rename a user")
-        .button("Back", |s| {
+        .on_event(Esc, |s| {
             s.pop_layer();
         }),
     )
-    .on_event(Esc, |s| {
-        s.pop_layer();
-    }))
 }
 
 fn remove_user(siv: &mut Cursive) {
-    let selected = siv.call_on_name("user_select", |view: &mut SelectView| {
-        let (slug, _user) = view.get_item(view.selected_id().expect("cannot get selected id")).expect("cannot get selected item");
+    let selected = siv
+        .call_on_name("user_select", |view: &mut SelectView| {
+            let (slug, _user) = view
+                .get_item(view.selected_id().expect("cannot get selected id"))
+                .expect("cannot get selected item");
 
-        slug.to_string()
-    })
-    .expect("cannot call user_select");
+            slug.to_string()
+        })
+        .expect("cannot call user_select");
 
     siv.add_layer(
         OnEventView::new(
-            Dialog::around(TextView::new(format!("You are going to delete {}", &selected)))
-                .title("Delete a user")
-                .button("Yes", move |s| {
-                    s.with_user_data(|data: &mut data::ProgramData| {
-                        data.user_list.remove(&selected);
-                    });
-                    s.pop_layer();
-                    s.pop_layer();
-                    user_list(s);
-                })
-                .button("Back", |s| {
-                    s.pop_layer();
-                }),
+            Dialog::around(TextView::new(format!(
+                "You are going to delete {}",
+                &selected
+            )))
+            .title("Delete a user")
+            .button("Yes", move |s| {
+                s.with_user_data(|data: &mut data::ProgramData| {
+                    data.user_list.remove(&selected);
+                });
+                s.pop_layer();
+                s.pop_layer();
+                user_list(s);
+            })
+            .button("Back", |s| {
+                s.pop_layer();
+            }),
         )
         .on_event(Esc, |s| {
             s.pop_layer();
