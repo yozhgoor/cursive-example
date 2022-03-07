f5e46fb9444a38828255650c28f9f72f1ea57bf2
yozhgoor <yozhgoor@outlook.com>
2022-03-07 15:54:20 +0100

    Init the logger and run a Cursive instance

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use cursive::{CursiveExt, Cursive};
+
+#[derive(Debug, clap::Parser)]
+struct Cli {
+    #[clap(long)]
+    log: bool,
+}
+
 fn main() {
-    println!("Hello World!");
+    let cli: Cli = clap::Parser::parse();
+
+    if cli.log {
+        env_logger::builder()
+            .filter(Some("tui_do_dev"), log::LevelFilter::Trace)
+            .format_timestamp(None)
+            .init();
+    }
+
+    let mut siv = Cursive::new();
+
+    siv.set_global_callback('q', |s| s.quit());
+
+    siv.run();
 }
